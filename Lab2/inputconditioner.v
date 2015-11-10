81f7eb2f0e41df64ac00ddabc4788ecc00807a62
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-11-09 23:52:19 -0500

    General comment improvement of inputconditioner.v

--- Lab2/inputconditioner.v
+++ Lab2/inputconditioner.v
@@ -14,27 +14,27 @@ output reg  positiveedge,   // 1 clk pulse at rising edge of conditioned
 output reg  negativeedge    // 1 clk pulse at falling edge of conditioned
 );
 
-    parameter counterwidth = 3; // Counter size, in bits, >= log2(waittime)
-    parameter waittime = 3;     // Debounce delay, in clock cycles
+    parameter counterwidth = 4; // Counter size, in bits, >= log2(waittime)
+    parameter waittime = 10;     // Debounce delay, in clock cycles
     
     reg[counterwidth-1:0] counter = 0;
     reg synchronizer0 = 0;
     reg synchronizer1 = 0;
     
     always @(posedge clk ) begin
-        if(conditioned == synchronizer1) begin// If the conditioned output is the same as the delayed input (2nd synchronizer), 
-            counter <= 0;
+        if(conditioned == synchronizer1) begin  // If the conditioned output is the same as the delayed input (2nd synchronizer), 
+            counter <= 0;                       // then nothing has changed, reset counter.
             positiveedge <= 0;
             negativeedge <= 0;
-        end                // then nothing has changed, reset counter.
+        end                                     
         else begin                              // However, if there has been a change,
-            if( counter == waittime) begin      // and we have waited for a sufficiently long time,
-                if (synchronizer1 > conditioned) begin
-                    positiveedge <= 1;
+            if( counter == waittime) begin      // and we have waited for a sufficiently long time, then we should change our conditioned output.
+                if (synchronizer1 > conditioned) begin  // Check whether our signal has moved from low -> high.
+                    positiveedge <= 1;                  // If it has, assert positive edge
                 end else begin
-                    negativeedge <= 1;
+                    negativeedge <= 1;                  // If it hasn't, assert negative edge [i.e. high -> low]
                 end
-                counter <= 0;                   // then reset the counter
+                counter <= 0;                   // Next reset the counter
                 conditioned <= synchronizer1;   // and set a new conditioned output.
             end
             else                                // If there has been a change, but we haven't waited long enough,
